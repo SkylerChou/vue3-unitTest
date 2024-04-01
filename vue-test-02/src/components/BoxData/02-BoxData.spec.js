e518531a3496b3d08b93286ba41fd49f76c39289
Skyler_Chou <skyler_chou@donkeymove.com>
2024-04-01 17:24:50 +0800

    3-2 : 手動設定資料： setData (Vue3 暫不適用)

--- vue-test-02/src/components/BoxData/02-BoxData.spec.js
+++ vue-test-02/src/components/BoxData/02-BoxData.spec.js
@@ -2,4 +2,16 @@ import { mount } from "@vue/test-utils";
 import BoxData from "@/components/BoxData";
 
 // https://github.com/vuejs/test-utils/issues/1058
-describe("BoxData.vue", () => {});
+describe("BoxData.vue", () => {
+  it("test v-show div", async () => {
+    const wraper = mount(BoxData);
+
+    /**
+     * 初始化之後再直接改變元件內的值(目前Vue3 conpisitionAPI 不支援，Vue2 optionAPI 才可以用)，
+     * 須注意 setData 是非同步的
+     */
+    await wraper.setData({ isOpen: false });
+
+    expect(wraper.find(".box_data").isVisible()).toBe(false);
+  });
+});
