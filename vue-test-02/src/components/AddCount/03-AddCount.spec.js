5084cf697a4261539ce3e48b5f6b21de105ee3dc
Skyler_Chou <skyler_chou@donkeymove.com>
2024-04-01 18:20:01 +0800

    3-3 : trigger 事件 click

--- vue-test-02/src/components/AddCount/03-AddCount.spec.js
+++ vue-test-02/src/components/AddCount/03-AddCount.spec.js
@@ -1,4 +1,25 @@
 import { shallowMount } from "@vue/test-utils";
 import AddCount from "@/components/AddCount";
 
-describe("AddCount.vue", () => {});
+describe("AddCount.vue", () => {
+  it("add number", async () => {
+    const wraper = shallowMount(AddCount);
+
+    /**
+     * trigger 是後天的動作，也是非同步的方法
+     */
+    await wraper.find(".add_btn").trigger("click");
+
+    expect(wraper.find(".count_title").text()).toBe("1");
+
+    await wraper.find(".add_btn").trigger("click");
+    expect(wraper.find(".count_title").text()).toBe("2");
+  });
+  it("remove number", async () => {
+    const wraper = shallowMount(AddCount);
+    await wraper.find(".remove_btn").trigger("click");
+    expect(wraper.find(".count_title").text()).toBe("-1");
+    await wraper.find(".remove_btn").trigger("click");
+    expect(wraper.find(".count_title").text()).toBe("-2");
+  });
+});
