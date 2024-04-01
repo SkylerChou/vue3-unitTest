396f74e562b0e565d20d91ea5cbc41615289b55e
Skyler_Chou <skyler_chou@donkeymove.com>
2024-04-01 18:29:03 +0800

    3-3 : 手動設定 props： setProps

--- vue-test-02/src/components/ContentBox/04-ContentBox.spec.js
+++ vue-test-02/src/components/ContentBox/04-ContentBox.spec.js
@@ -1,4 +1,15 @@
 import { mount } from "@vue/test-utils";
 import ContentBox from "@/components/ContentBox";
 
-describe("ContentBox.vue", () => {});
+describe("ContentBox.vue", () => {
+  it("test props", async () => {
+    const wraper = mount(ContentBox);
+
+    /**
+     * setup 裡面的東西都不能用 set 相關方法設定
+     */
+    await wraper.setProps({ isOpen: false });
+
+    expect(wraper.find(".box").isVisible()).toBe(false);
+  });
+});
