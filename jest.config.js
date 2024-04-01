07b596e817e9e2bc250f949865554bb1d90f43b9
Skyler_Chou <skyler_chou@donkeymove.com>
2024-04-01 11:53:25 +0800

    feat : 第 2 章 - 常用的 Vue-test-utils 語法 - 建置測試元件

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
   testMatch: ["**/src/**/*.spec.[jt]s?(x)"],
-  moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
+  moduleFileExtensions: ["js", "jsx", "json", "vue"],
 };
