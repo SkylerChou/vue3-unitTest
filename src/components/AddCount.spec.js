0cf3f616cdfed68d3f7b59ad1e1cc9f1edd957bb
Skyler_Chou <skyler_chou@donkeymove.com>
2024-04-01 15:05:25 +0800

    chore : 修正eslint 設定

--- src/components/AddCount.spec.js
+++ src/components/AddCount.spec.js
@@ -4,11 +4,11 @@ import AddCount from "@/components/AddCount.vue";
 describe("AddCount.vue test", () => {
   it("test 1", () => {
     const wrapper = shallowMount(AddCount);
-  /**
-    * find => 如果找不到元素不會報錯 ( 結果報錯 )
-    * get => 如果找不到元素會報錯 ( 中斷測試執行 )
-    */
+    /**
+     * find => 如果找不到元素不會報錯 ( 結果報錯 )
+     * get => 如果找不到元素會報錯 ( 中斷測試執行 )
+     */
     // expect(wrapper.get(".add-btn-error").exists()).toBe(true)
-    expect(wrapper.find(".add-btn").exists()).toBe(true)
+    expect(wrapper.find(".add-btn").exists()).toBe(true);
   });
 });
