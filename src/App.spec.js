b33fba04961a4749b9d6cbb25ad4349a75c288e3
Skyler_Chou <skyler_chou@donkeymove.com>
2024-04-01 14:15:06 +0800

    2-1 : mount 與 shallowMount 的差異

---
+++ src/App.spec.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+import { mount, shallowMount } from "@vue/test-utils";
+import App from "@/App.vue";
+
+describe("App.vue test", () => {
+  it("測試元件 1 - 淺渲染", () => {
+    /** 淺渲染只會抓父元件(第一層) */
+    const wrapper = shallowMount(App);
+    console.log(wrapper.text());
+
+    expect(wrapper.text()).toMatch("Test demo");
+  });
+
+  it("測試元件 2 - 深度渲染", () => {
+    /** 深度選染會抓父元件跟子元件 */
+    const wrapper = mount(App);
+    console.log(wrapper.text());
+
+    expect(wrapper.text()).toMatch("Test demo");
+  });
+});
