ba66d144c3617de699a857ab7a93f9eb72927316
Skyler_Chou <skyler_chou@donkeymove.com>
2024-04-02 15:45:05 +0800

    測試範例 - 攔截其他第三方函式 - jest.mockjest.mock 其他函式

--- vue-test-04/src/components/TimeShow.spec.js
+++ vue-test-04/src/components/TimeShow.spec.js
@@ -1,4 +1,17 @@
 import { mount } from "@vue/test-utils";
 import TimeShow from "./TimeShow.vue";
 
-describe("TimeShow.vue", () => {});
+/**
+ * 透過 jest.mock 攔截第三方函式的內容，來測試所攔截的函是執行結果
+ */
+jest.mock("../lib", () => ({
+  secondsFormat: jest.fn(() => "00:00:00"),
+}));
+
+describe("TimeShow.vue", () => {
+  it("test lib", () => {
+    const wrapper = mount(TimeShow);
+
+    expect(wrapper.find("h1").text()).toBe("time: 00:00:00");
+  });
+});
