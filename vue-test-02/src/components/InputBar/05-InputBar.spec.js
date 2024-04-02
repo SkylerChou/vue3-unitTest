e1b9fa07c79c2f99e523397afd50c9114abf047c
Skyler_Chou <skyler_chou@donkeymove.com>
2024-04-02 10:47:51 +0800

    3-4 : 手動設定表單value： setValue

--- vue-test-02/src/components/InputBar/05-InputBar.spec.js
+++ vue-test-02/src/components/InputBar/05-InputBar.spec.js
@@ -1,4 +1,19 @@
 import { mount } from "@vue/test-utils";
 import InputBar from "@/components/InputBar";
 
-describe("InputBar.vue", () => {});
+describe("InputBar.vue", () => {
+  it("test input data", () => {
+    const wraper = mount(InputBar);
+    wraper.find("input.name").setValue("Skyler");
+    wraper.find("input.email").setValue("Skyler@gmail.com");
+    expect(wraper.find("input.name").element.value).toBe("Skyler");
+    expect(wraper.find("input.email").element.value).toBe("Skyler@gmail.com");
+  });
+
+  it("test thousand", async () => {
+    const wraper = mount(InputBar);
+    await wraper.setProps({ thousand: 1345000 });
+
+    expect(wraper.find("input.num").element.value).toBe("1,345,000");
+  });
+});
