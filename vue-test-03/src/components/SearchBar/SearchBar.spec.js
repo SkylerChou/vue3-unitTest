e8c05ef755765f08bd26e8ec2efac475a8d1b081
Skyler_Chou <skyler_chou@donkeymove.com>
2024-04-02 11:13:33 +0800

    4-1 : 測試範例 - 搜尋頁面 ( toBe 和 forEach 的運用 )

---
+++ vue-test-03/src/components/SearchBar/SearchBar.spec.js
@@ -0,0 +1,23 @@
+import { mount } from "@vue/test-utils";
+import SearchBar from "@/components/SearchBar";
+
+describe("SearchBar.vue", () => {
+  it("test demo1", async () => {
+    const wrapper = mount(SearchBar);
+
+    expect(wrapper.find(".title").text()).toBe(
+      "Vue 單元測試 vue-test-utils｜入門"
+    );
+
+    await wrapper.find(".text3").trigger("click");
+
+    const titleArr = [
+      "館長成吉思汗- YouTube",
+      "陳之漢- 维基百科，自由的百科全書",
+    ];
+
+    wrapper.findAll(".title").forEach((item, idx) => {
+      expect(item.text()).toBe(titleArr[idx]);
+    });
+  });
+});
