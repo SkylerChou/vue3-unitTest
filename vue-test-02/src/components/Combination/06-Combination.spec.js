68c5382da409a054a85146a6ac8a2f136bd63d24
Skyler_Chou <skyler_chou@donkeymove.com>
2024-04-02 10:53:56 +0800

    3-5 : emitted 的觸發

--- vue-test-02/src/components/Combination/06-Combination.spec.js
+++ vue-test-02/src/components/Combination/06-Combination.spec.js
@@ -1,4 +1,12 @@
 import { mount } from "@vue/test-utils";
 import Combination from "@/components/Combination";
 
-describe("Combination.vue", () => {});
+describe("Combination.vue", () => {
+  it("test emit", async () => {
+    const wraper = mount(Combination);
+    await wraper.setProps({ thousand: 1000000 });
+    await wraper.find("button").trigger("click");
+    console.log(wraper.emitted("Combination"));
+    expect(wraper.emitted("Combination").at(0).at(0)).toBe("1,000,000");
+  });
+});
