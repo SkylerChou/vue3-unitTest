cf5efa0d19a95ed8076e7cf60b42c8eb9694fbce
Skyler_Chou <skyler_chou@donkeymove.com>
2024-04-09 16:32:15 +0800

    7-1 : stubs 掛載 router-link & router-view

--- vue-test-06/src/App.spec.js
+++ vue-test-06/src/App.spec.js
@@ -3,6 +3,11 @@ import App from "./App.vue";
 
 describe("App.vue", () => {
   it("route test", () => {
-    const wrapper = mount(App);
+    const wrapper = mount(App, {
+      global: {
+        // 讓測試不會報全域掛載元件的錯，設定全域掛載
+        stubs: ["router-link", "router-view"],
+      },
+    });
   });
 });
