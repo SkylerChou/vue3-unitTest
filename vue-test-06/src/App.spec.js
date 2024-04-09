7c8445fa9c1a9b8256f0cdd9d9ec7fe737353f4a
Skyler_Chou <skyler_chou@donkeymove.com>
2024-04-09 16:59:56 +0800

    7-2 : 綁架 useRoute 和 useRouter

--- vue-test-06/src/App.spec.js
+++ vue-test-06/src/App.spec.js
@@ -1,6 +1,10 @@
 import { mount } from "@vue/test-utils";
 import App from "./App.vue";
 
+jest.mock("vue-router", () => ({
+  useRoute: jest.fn(() => ({ path: "about" })),
+}));
+
 describe("App.vue", () => {
   it("route test", () => {
     const wrapper = mount(App, {
@@ -9,5 +13,7 @@ describe("App.vue", () => {
         stubs: ["router-link", "router-view"],
       },
     });
+    console.log(wrapper.html());
+    expect(wrapper.find("#about").classes("active")).toBe(true);
   });
 });
