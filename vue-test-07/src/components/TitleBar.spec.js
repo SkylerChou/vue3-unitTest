41b156cab251483340b6b95b1d2e251a25ac47df
Skyler_Chou <skyler_chou@donkeymove.com>
2024-04-11 11:23:42 +0800

    8-1 : slots

---
+++ vue-test-07/src/components/TitleBar.spec.js
@@ -0,0 +1,21 @@
+import { mount } from "@vue/test-utils";
+import TitleBar from "./TitleBar.vue";
+
+describe("TitleBar.vue", () => {
+  it("renders slots content", () => {
+    const wrapper = mount(TitleBar, {
+      slots: {
+        default: "今天天氣很好",
+        detail: "這是複製的",
+      },
+    });
+
+    expect(wrapper.find(".title").html()).toBe(
+      '<h1 class="title">今天天氣很好</h1>'
+    );
+
+    expect(wrapper.find(".detail").html()).toBe(
+      '<p class="detail">這是複製的</p>'
+    );
+  });
+});
